727a98389a3f439a4ab076348bf8d01e06908d1a
Krishna-Aditya <49936001+Krishna-Aditya@users.noreply.github.com>
2020-03-27 15:05:40 +0530

    login & registration added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegComponent } from './reg/reg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegComponent
   ],
   imports: [
     BrowserModule,
